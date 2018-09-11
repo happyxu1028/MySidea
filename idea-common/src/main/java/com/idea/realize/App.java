9c94c459f876930a538a4123dec0bff9003babc6
happyxu1028 <happyxu1028@163.com>
2018-09-11 23:09:40 +0800

    a

---
+++ idea-common/src/main/java/com/idea/realize/App.java
@@ -0,0 +1,13 @@
+package com.idea.realize;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
