1bc2149fb13e510ccb1ee817f8c556e869c02d78
happyxu1028 <happyxu1028@163.com>
2018-09-11 23:27:53 +0800

    搭建web start

---
+++ idea-start/src/main/java/com/idea/realize/SideaApplication.java
@@ -0,0 +1,22 @@
+package com.idea.realize;
+
+//import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author happyxu
+ * @Description: TODO
+ * @Title: SideaApplication
+ * @ProjectName sidea
+ * @date 2018/9/11下午11:16
+ */
+@SpringBootApplication
+//@MapperScan("com.idea.realize")
+public class SideaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideaApplication.class, args);
+    }
+
+}
