a948264d7efda5e8622271741f82a153885de92d
happyxu1028 <happyxu1028@163.com>
2018-11-12 21:14:22 +0800

    冒泡排序,插入排序,选择排序算法的实现

---
+++ idea-service/src/main/java/com/idea/realize/sort/SelectSortDemo.java
@@ -0,0 +1,30 @@
+package com.idea.realize.sort;
+
+import java.util.Arrays;
+
+public class SelectSortDemo {
+
+    public static void main(String[] args) {
+        selectSort(new int[]{1,435,16,2,6,346});
+    }
+
+
+    public static void selectSort(int[] a){
+        System.out.println(Arrays.toString(a));
+        for(int i =0; i < a.length; i++){
+            int index = i;
+            for(int j = i+1; j < a.length; j++){
+                if(a[j] < a[index]){
+                    index = j;
+                }
+            }
+
+            if(i != index){
+                int temp = a[i];
+                a[i] = a[index];
+                a[index] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
